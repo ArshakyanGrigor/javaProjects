919d00b840499b52846ab4b248684f0194b7e029
ArshakyanGrigor <arshakiyangregory@gmail.com>
2021-10-14 12:35:44 +0400

    Double Linked List and Deque

---
+++ DoubleLinkedList/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyDoubleLinkedList myLinkedList = new MyDoubleLinkedList();
+        myLinkedList.addFirst(3);
+        myLinkedList.addLast(4);
+        myLinkedList.addLast(7);
+        myLinkedList.addLast(3);
+        myLinkedList.addFirst(4);
+
+    }
+}
